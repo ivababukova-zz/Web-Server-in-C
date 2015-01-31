df62eee1b2029641e0f91524042adf193dc3dffd
ivababukova <ibabukova@gmail.com>
2015-01-31 20:00:55 +0000

    Reads and finds the file requested and opens it successfuly. However, there is bug in closing the sockets.

--- web-server.c
+++ web-server.c
@@ -12,7 +12,7 @@
 
 #define MAXLEN 1024 // maximum length of the buffer
 #define MY_PORT 8080 // the port I am using
-
+#define MAXFILENAME 30 // the max lenght of the name of the file requested
 
 // don't put everything in main!
 // functions that parse the request:
@@ -20,13 +20,49 @@
 // 1. retrieve pointer to the name of the file requested:
 
 
+// returns the file requested by the browser: 
+char * parser (char *buff) {
+    char *token = NULL;
+    static char filename[MAXFILENAME];
+    char *p;
+    int i, j;
+
+    token = strtok(buff, "\n");
+
+    while (token) {
+        if ( token[0] == 'G' && (2)<strlen(token)) {
+            if (token[1] == 'E' && token[2] == 'T') { 
+                i = 5;
+                j = 0;
+                while (token[i] != ' ') {
+                    filename[j] = token[i];
+                    j++;
+                    i++;
+                }
+                filename[j] = '\0';
+                break;
+            }
+        }
+        token = strtok(NULL, "\n");
+    }
+
+    p = filename;
+    // check whether the filename is correctly parsed: 
+    return p;
+}
+
+
 int main() {
 
     struct sockaddr_in addr;
     struct sockaddr_in cliaddr;
-    int sock, clientsock, listens, connsocket;
+    int sock, clientsock, listens;
     socklen_t cliaddrlen;
-    char buff[MAXLEN];
+    char buff[MAXLEN] = {0};
+    char *p; // pointer to the buffer that contains the http request
+    char *filerequest;  // pointer to the filename requested
+    p = buff;
+    FILE *fp;
 
     // create a TCP socket
     sock = socket(AF_INET, SOCK_STREAM, 0); 
@@ -49,22 +85,39 @@ int main() {
         return -1;
     }
  
-    // accepting request from the browser:
     cliaddrlen = sizeof(cliaddr);
-
-    connsocket = accept (sock, (struct sockaddr*) &cliaddr, &cliaddrlen);
-    if (connsocket == -1 ) {
-        return -1;
-    }
 
     while(1) {
     // read():
         // accepts the connection, returns new file descriptor for the connection and cliaddr
         clientsock = accept(sock, (struct sockaddr*)&cliaddr, &cliaddrlen);
         printf ("connected\n");
-        send(clientsock, buff, recv(clientsock, buff, MAXLEN, 0), 0);
-        close(clientsock);
-    }
+        int byte_count = recv(clientsock, buff, MAXLEN, 0);
+        send(clientsock, buff, byte_count, 0);
+        filerequest = parser (p);
+        printf ("result from parsing: %s\n", filerequest);
+        int c;
+        fp = fopen(filerequest, "r");
+        if (fp == NULL) {
+            printf("error while opening file.\n");
+        }
+        
+
+        while (1) {
+            unsigned char data[256] = {0};
+            int newlen;
+            if (fp) {
+                newlen = fread (data, sizeof(char), 256, fp);
+                data[++newlen] = '\0';   
+                fclose(fp);
+            }
+            printf("Sending...\n");
+            write (clientsock, data, newlen);
+            int i = 0;
+            close(clientsock);
+            }
+        }
+
     close(sock);
     return (0);
 }
